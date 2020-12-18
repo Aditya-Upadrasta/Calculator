d3d9d81893396c76cea574d189ff8471ffeabafd
Aditya-Upadrasta <76166104+Aditya-Upadrasta@users.noreply.github.com>
2020-12-18 11:24:32 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,8 +7,8 @@ prob = input('What do you want to do??:  \n add\n subtract\n multiply\n divide\n
 
 sum = float(num1) + float(num2)
 sub = float(num1) - float(num2)
-product = float(num1) * float(num2)
 div = float(num1) / float(num2)
+product = float(num1) * float(num2)
 
 if prob == 'add':
     print('The sum of {0} and {1} is {2}' .format(num1,num2,sum))    
