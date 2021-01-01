3ad2677efe549728761d5d5e31578c1afc1c3781
Aditya-Upadrasta <76166104+Aditya-Upadrasta@users.noreply.github.com>
2021-01-01 17:12:54 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,22 +2,22 @@
 num1 = input('Enter your 1st number: ')
 num2 = input('Enter your 2nd number: ')
 
-prob = input('What do you want to do??:  \n add\n subtract\n multiply\n divide\n : ')
+prob = input('What do you want to do??: (+)\t (-)\t (*)\t (/)\n \t  \t \t \t \t \t:-  ')
 
 
 sum = float(num1) + float(num2)
 sub = float(num1) - float(num2)
 div = float(num1) / float(num2)
-product = float(num1) * float(num2)
+multiply = float(num1) * float(num2)
 
-if prob == 'add':
+if prob == '+':
     print('The sum of {0} and {1} is {2}' .format(num1,num2,sum))    
     
-elif prob == 'subtract':
+elif prob == '-':
     print('The subtraction of {0} and {1} is {2}' .format(num1,num2,sub))
     
-elif prob == 'product':
-    print('The product of {0} and {1} is {2}' .format(num1,num2,product))
+elif prob == '*':
+    print('The multiply of {0} and {1} is {2}' .format(num1,num2,multiply))
 
 else:
     print('The division of {0} and {1} is {2}' .format(num1,num2,div))
